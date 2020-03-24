0e831a3d88cb70f834ff71ba20961c6be8ec086d
yuta <yuta@yutanoMacBook-ea.local>
2020-03-25 02:00:26 +0900

    各ファイルの更新2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import io
 import csv
 import pprint
+import datetime
 from flask import Flask, render_template, request, jsonify
 from wtforms import Form, StringField, SubmitField, validators, ValidationError
 import numpy as np
@@ -9,10 +10,21 @@ from flask_table import Table, Col
 
 app = Flask(__name__)
 
-class Item(object):
+allmemberlist = ['Kazuki_Egashira', 'Shiho_Aoki', 'Yusuke_Kimura', 'Yuta_Hosokawa']
+memberlist = []
+attendance = {}
+attendTime = {}
+exitTime = {}
+
+class AtdItem(object):
     def __init__(self, name, description1):
         self.name = name
         self.description1 = description1
+
+        
+class NotAtdItem(object):
+    def __init__(self, name):
+        self.name = name
         
 
 class LogItem(object):
@@ -22,15 +34,19 @@ class LogItem(object):
         self.description2 = description2
 
 
-class ItemTable(Table):
+class AtdTable(Table):
     name = Col('名前')
     description1 = Col('出席時刻')
+
+
+class NotAtdTable(Table):
+    name = Col('名前')
     
 
-class LogItemTable(Table):
+class LogTable(Table):
     name = Col('名前')
     description1 = Col('時刻')
-    description1 = Col('入退状況')
+    description2 = Col('入退状況')
 
 
 class addUser(Form):
@@ -38,15 +54,22 @@ class addUser(Form):
                            [validators.InputRequired("この項目は入力必須です")])
 
     # html側で表示するsubmitボタンの表示
-    submit = SubmitField("追加")
+    submit = SubmitField("決定")
 
-memberlist = []
-attendance = {}
-attendTime = {}
-exitTime = {}
-logdictlist = []
 
-def update_dict(logdictlist):
+def read_csv():
+    with open('face_log.csv') as f:
+        reader = csv.DictReader(f, fieldnames=['name', 'time', 'state'])
+        logdictlist = [row for row in reader]
+
+    loglength = len(logdictlist)
+    for i in range(loglength):
+        logdictlist[i]['time'] = datetime.datetime.strptime(logdictlist[i]['time'], '%Y%m%d%H%M%S')
+
+    return logdictlist
+
+
+def update_log(logdictlist):
     d_len = len(logdictlist)
 
     for i in range(d_len):
@@ -65,64 +88,66 @@ def index1():
 
 @app.route('/conf/')
 def index2():
-    items = []
+    atditems = []
+    natditems = []
     length = len(memberlist)
     
     if length == 0:
         return render_template('index2_n.html')
 
-    with open('face_log.csv') as f:
-        reader = csv.DictReader(f)
-        logdictlist = [row for row in reader]
+    logdictlist = read_csv()
 
-    loglength = len(logdictlist)
-    print(loglength)
-
-    update_dict(logdictlist)
+    update_log(logdictlist)
 
     for i in range(length):
         name = memberlist[i]
         if attendance[name]:
-            items.append(Item(name, attendTime[name]))
+            atditems.append(AtdItem(name, attendTime[name]))
+        else:
+            natditems.append(NotAtdItem(name))
             
-    table = ItemTable(items)
-    return render_template('index2.html', table=table)
+    atdtable = AtdTable(atditems)
+    natdtable = NotAtdTable(natditems)
+    return render_template('index2.html', table1=atdtable, table2=natdtable)
 
 @app.route('/log/')
 def index2_log():
     logitems = []
 
-    with open('face_log.csv') as f:
-        reader = csv.DictReader(f)
-        logdictlist = [row for row in reader]
+    logdictlist = read_csv()
 
     loglength = len(logdictlist)
     print(loglength)
 
     for i in range(loglength):
-        logitems.append(LogItem(logdictlist[i]['name'], logdictlist[i]['time'], logdictlist[i]['state']))
+        logitems.append(LogItem(logdictlist[i]['name'], logdictlist[i]['time'], logdictlist[i]['state'].upper()))
 
-    logtable = LogItemTable(logitems)
+    logtable = LogTable(logitems)
     return render_template('index2_log.html', logtable=logtable)
 
 @app.route('/create/', methods = ['GET', 'POST'])
 def index3():
-    form = addUser(request.form)
     if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('index3.html', form=form)
-        else:
-            memberlist.append(request.form["username"])
-            attendance[request.form["username"]]=False
-            attendTime[request.form["username"]]='-'
-            exitTime[request.form["username"]]='-'
+        member_index = request.form.getlist('checkbox')
+        member_index_i = [int(s) for s in member_index]
 
-            print(memberlist)
+        if len(allmemberlist) in member_index_i:
+            member_index_i = range(len(allmemberlist))
+        ilen = len(member_index_i)
+        print(member_index_i)
 
-            return render_template('index3.html', form=form)
+        for i in range(ilen):
+            memberlist.append(allmemberlist[member_index_i[i]])
+            attendance[allmemberlist[member_index_i[i]]]=False
+            attendTime[allmemberlist[member_index_i[i]]]='-'
+            exitTime[allmemberlist[member_index_i[i]]]='-'
+        
+        print(memberlist)
+        
+        return render_template('index3.html', message = '出席簿を作成しました、トップページに戻ってください', member=allmemberlist)
     elif request.method == 'GET':
         
-        return render_template('index3.html', form=form)
+        return render_template('index3.html', message = '今回の出席予定者を全て選択して下さい', member=allmemberlist)
 
 @app.route('/camera/')
 def index4():
